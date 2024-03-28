3891e3c8510464d4f285e4e667902c9205782717
sanjayvjacob <sanjayvinodjacob@gmail.com>
2024-03-28 14:10:02 +0000

    express server

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello!<h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>Sanjay Jacob</h1><p>Software Developer</p>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>phone</h1><p>123455</p>");
+});
+
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
+});
